cc8f7daa94994b7d941b402fdea7ad5f35103473
galibgaurav <galibgaurav@gmail.com>
2015-12-18 12:56:46 -0800

    user registration

--- User.cs
+++ User.cs
@@ -15,24 +15,29 @@ namespace MyStartUpMate
         [StringLength(50)]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Please Provide Last Name",AllowEmptyStrings =false)]
         [StringLength(50)]
         public string LastName { get; set; }
 
+        [Display(Name = "Password")]
         [Required(ErrorMessage = "Please Provide Last Name", AllowEmptyStrings = false)]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
         [StringLength(50,MinimumLength =8,ErrorMessage ="Password must be 8 char long. ")]
         public string Password { get; set; }
 
+        [Display(Name = "Confirm Password")]
         [Compare("Password", ErrorMessage = "Confirm password does not match. ")]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
         public string ConfirmPassword { get; set; }
 
+        [Display(Name = "Email")]
         [Required(ErrorMessage = "Please Provide Email Address", AllowEmptyStrings = false)]
         [StringLength(50)]
         [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",ErrorMessage ="Please provide a valid email id")]
         public string Email { get; set; }
 
+        [Display(Name="Mobile Number")]
         [Required(ErrorMessage ="Please provide mobile number")]
         [StringLength(10)]
         [RegularExpression("^[7-9][0-9]{9}$",ErrorMessage = "Please provide a valid mobile number")]
